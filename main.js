9b77e6963ab775a43ecc1b2335245089e70a8c5e
egyption-pharhon <egyptionpharhon@gmail.com>
2024-09-17 02:11:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// open list items @ media 767px
+
+function openNavList(){
+	let navlist = document.querySelector('nav');
+	navlist.classList.toggle("show");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
